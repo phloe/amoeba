69fea874e4d763d25e342e0bff62864e342c26c7
Rasmus Fløe <rasmusfl0e@gmail.com>
2011-05-20 17:16:06 +0200

    QSA

--- bootstrap-min.js
+++
@@ -1,1 +0,0 @@
-(function(a,b,c,d,e){e=b.getElementsByTagName("head")[0],c=a.length,d=function(f){if(c){f=b.createElement("script");f.onload=d;f.onreadystatechange=function(){if(f.readyState=="loaded")d()}f.src=a[--c];e.appendChild(f)}}d()})(["{arguments}"],document);
