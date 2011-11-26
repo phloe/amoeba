99a25cea969b543b5a038f0610fd58769fdd251f
Rasmus Fløe <rasmusfl0e@gmail.com>
2011-11-27 00:45:31 +0100

    Update bootstrap_chain.js

--- bootstrap_chain.js
+++ bootstrap_chain.js
@@ -1,30 +1,31 @@
-(function(scripts, document, index, tag, firstScript, load, state){
+(function(scripts, document){
 
-	tag = "script";
-
-	index = scripts.length;
-
-	firstScript = document.getElementsByTagName(tag)[0];
-
-	load = function (script) {
-		if (index) {
-			script = document.createElement(tag);
-            state = script.onreadystatechange;
-			if (state !== undefined) {
-				state = function () {
-					if (script.readyState == "loaded") {
-						load();
-					}
-				};
+	var state, 
+		tag = "script",
+	
+		index = scripts.length,
+	
+		firstScript = document.getElementsByTagName(tag)[0],
+	
+		load = function (script) {
+			if (index) {
+				script = document.createElement(tag);
+	            state = script.onreadystatechange;
+				if (state !== undefined) {
+					state = function () {
+						if (script.readyState == "loaded") {
+							load();
+						}
+					};
+				}
+				else {
+					script.onload = load;
+				}
+				script.src = scripts[--index];
+				firstScript.parentNode.insertBefore(script, firstScript);
+				firstScript = script;
 			}
-			else {
-				script.onload = load;
-			}
-			script.src = scripts[--index];
-			firstScript.parentNode.insertBefore(script, firstScript);
-			firstScript = script;
-		}
-	};
+		};
 
 	load();
 
