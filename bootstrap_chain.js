e512088e569a5aa19335e487f4b7c0722313a3db
Rasmus Fløe <rasmusfl0e@gmail.com>
2012-03-02 21:08:01 +0100

    IE onreadystatechange fix

--- bootstrap_chain.js
+++ bootstrap_chain.js
@@ -10,10 +10,10 @@
 	(function load (script) {
 		if (index) {
 			script = doc.createElement(tag);
-            state = script.onreadystatechange;
-			if (state !== undefined) {
-				state = function () {
-					if (script.readyState == "loaded") {
+			if (script.readyState) {
+				script.onreadystatechange = function () {
+					if (script.readyState == "loaded" || script.readyState == "complete") {
+						script.onreadystatechange = null;
 						load();
 					}
 				};
