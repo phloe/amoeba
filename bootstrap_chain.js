35fe74b87ed820412ad8dd98ec8c753adb5b9f70
Rasmus Fløe <rasmusfl0e@gmail.com>
2012-04-11 04:38:39 +0200

    Added tags to fork strategies

--- bootstrap_chain.js
+++ bootstrap_chain.js
@@ -1,25 +1,46 @@
 (function(scripts, doc){
 
 	var tag = "script",
-	
 		index = scripts.length,
-	
-		firstScript = doc.getElementsByTagName(tag)[0];
+		firstScript = 
+			// <featureDetect>
+			(doc.querySelector) ? 
+			// </featureDetect>
+			// <querySelector>
+				doc.querySelector(tag)
+			// </querySelector>
+			// <featureDetect>
+			: 
+			// </featureDetect>
+			// <getElementsByTagName>
+				doc.getElementsByTagName(tag)[0]
+			// </getElementsByTagName>
+			;
 	
 	(function load (script) {
 		if (index) {
 			script = doc.createElement(tag);
+			// <featureDetect>
 			if (script.readyState) {
+			// </featureDetect>
+			// <readyState>
 				script.onreadystatechange = function () {
 					if (script.readyState == "loaded" || script.readyState == "complete") {
 						script.onreadystatechange = null;
 						load();
 					}
 				};
+			// </readyState>
+			// <featureDetect>
 			}
 			else {
+			// </featureDetect>
+			// <onload>
 				script.onload = load;
+			// </onload>
+			// <featureDetect>
 			}
+			// </featureDetect>
 			firstScript.parentNode.insertBefore(script, firstScript);
 			script.src = scripts[--index];
 			firstScript = script;
