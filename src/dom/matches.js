e07686e1ff0d1eebb139e6a36ab6d18bfd1400f1
Rasmus Fløe <rasmusfl0e@gmail.com>
2015-05-21 01:36:02 +0200

    Update matches.js

--- src/dom/matches.js
+++ src/dom/matches.js
@@ -1,5 +1,17 @@
 var html = document.documentElement;
-var matches = html.matches || html.matchesSelector || html.mozMatchesSelector || html.webkitMatchesSelector || html.msMatchesSelector || html.oMatchesSelector;
+var matches = html.matches || html.matchesSelector || html.mozMatchesSelector ||
+	html.webkitMatchesSelector || html.msMatchesSelector || html.oMatchesSelector ||
+	function (selector) {
+		var elements = this.parentNode.querySelectorAll(selector);
+		var length = elements.length;
+		var i = 0;
+		while (i < length) {
+			if (elements[i++] === this) {
+				return true;
+			}
+		}
+		return false;
+	};
 
 module.exports = function (element, selector) {
 	return matches.call(element, selector);
